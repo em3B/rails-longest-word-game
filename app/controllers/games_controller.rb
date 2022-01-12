1f533ea1bee85f264060bd6f4addaaefa7a9ab61
Emily Baird <emilym.baird@gmail.com>
2022-01-12 14:32:10 +0000

    all mandatory tasks coomplete

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,13 @@
+require "json"
+require "open-uri"
+
 class GamesController < ApplicationController
   def new
+    @letters = ("A".."Z").to_a.shuffle[0..10]
   end
   def score
-    raise
+    @url = "https://wagon-dictionary.herokuapp.com/#{params[:answer]}"
+    @user_serialized = URI.open(@url).read
+    @user = JSON.parse(@user_serialized)
   end
 end
